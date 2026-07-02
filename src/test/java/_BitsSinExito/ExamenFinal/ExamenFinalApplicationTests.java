3c68c9103047918383ad0e2383f20592277d2d51
Dafne Tapia <romidafne23@gmail.com>
2026-07-02 01:13:36 -0400

    creando proyecto para el bakend , y carpetas necesarias

---
+++ src/test/java/_BitsSinExito/ExamenFinal/ExamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package _BitsSinExito.ExamenFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
