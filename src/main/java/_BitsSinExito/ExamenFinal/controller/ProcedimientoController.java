778326d4ef4f993e72bd640d8ed76d42cc4e9eaf
Melani Hidalgo <marcahidalgo7@gmail.com>
2026-07-02 20:04:48 -0400

    agrego endpoints CRUD para procedimientos

--- src/main/java/_BitsSinExito/ExamenFinal/controller/ProcedimientoController.java
+++ src/main/java/_BitsSinExito/ExamenFinal/controller/ProcedimientoController.java
@@ -2,24 +2,44 @@ package _BitsSinExito.ExamenFinal.controller;
 
 import _BitsSinExito.ExamenFinal.model.Procedimiento;
 import _BitsSinExito.ExamenFinal.service.ProcedimientoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/procedimientos")
+@CrossOrigin(origins = "*")
 public class ProcedimientoController {
 
-    @Autowired
-    private ProcedimientoService procedimientoService;
+    private final ProcedimientoService procedimientoService;
+
+    public ProcedimientoController(ProcedimientoService procedimientoService) {
+        this.procedimientoService = procedimientoService;
+    }
 
     @GetMapping
     public List<Procedimiento> listarTodos() {
         return procedimientoService.listarTodos();
     }
 
-    @GetMapping("/servicio/{servicioId}")
-    public List<Procedimiento> listarPorServicio(@PathVariable Long servicioId) {
-        return procedimientoService.listarPorServicio(servicioId);
+    @GetMapping("/{id}")
+    public Procedimiento buscarPorId(@PathVariable Long id) {
+        return procedimientoService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Procedimiento guardar(@RequestBody Procedimiento procedimiento) {
+        return procedimientoService.guardar(procedimiento);
+    }
+
+    @PutMapping("/{id}")
+    public Procedimiento actualizar(@PathVariable Long id,
+                                    @RequestBody Procedimiento procedimiento) {
+        return procedimientoService.actualizar(id, procedimiento);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        procedimientoService.eliminar(id);
     }
 }
