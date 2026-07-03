05dc3da3bd6f09ec79320555acf30e711aa9b701
Melani Hidalgo <marcahidalgo7@gmail.com>
2026-07-02 20:07:05 -0400

    agrego la logica para guardar editar eliminar y buscar procedimientos

--- src/main/java/_BitsSinExito/ExamenFinal/service/ProcedimientoService.java
+++ src/main/java/_BitsSinExito/ExamenFinal/service/ProcedimientoService.java
@@ -4,6 +4,7 @@ import _BitsSinExito.ExamenFinal.model.Procedimiento;
 import _BitsSinExito.ExamenFinal.repository.ProcedimientoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -14,6 +15,23 @@ public class ProcedimientoService {
 
     public List<Procedimiento> listarTodos() {
         return procedimientoRepository.findAll();
+    }
+
+    public Procedimiento buscarPorId(Long id) {
+        return procedimientoRepository.findById(id).orElse(null);
+    }
+
+    public Procedimiento guardar(Procedimiento procedimiento) {
+        return procedimientoRepository.save(procedimiento);
+    }
+
+    public Procedimiento actualizar(Long id, Procedimiento procedimiento) {
+        procedimiento.setId(id);
+        return procedimientoRepository.save(procedimiento);
+    }
+
+    public void eliminar(Long id) {
+        procedimientoRepository.deleteById(id);
     }
 
     public List<Procedimiento> listarPorServicio(Long servicioId) {
